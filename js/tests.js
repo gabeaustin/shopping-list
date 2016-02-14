3e58b62f6598af645b3b86a265fb62021a156baf
Gabe Austin <gabriel.austin1@gmail.com>
2016-02-13 22:32:00 -0600

    add remove items icon & jquery ui

--- js/tests.js
+++ js/tests.js
@@ -1,3 +1,7 @@
-QUnit.test( "hello test", function( assert ) {
-  assert.ok( 1 == "1", "Test Passed!" );
+// QUnit.test( "hello test", function( assert ) {
+//   assert.ok( 1 == "1", "Test Passed!" );
+// });
+
+QUnit.test( "input equal test", function( assert ) {
+  assert.equal( $('.item'), $('.listOfItemsCheckbox'), "Sting input and input item have the same value" );
 });
