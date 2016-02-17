f5a40f693d25d17796c1807a3b49f9c5f3e22aa2
Gabe Austin <gabriel.austin1@gmail.com>
2016-02-16 23:43:17 -0600

    add checkbox dynamically

--- js/script.js
+++ js/script.js
@@ -1,3 +1,12 @@
 // $(document).ready(function() {
 //   $(".item").toggle("shake");
 // })
+
+$(document).ready(function() {
+  $( "input" )
+    .keyup(function() {
+      var value = $( this ).val();
+      $( "li" ).html( "<input type='checkbox'>" + value + "</input><br>" )
+      .addClass("listOfItemsCheckbox");
+    });
+});
