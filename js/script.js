9740474d9e723224b4b8b575b727fce1d2b762f0
Gabe Austin <gabriel.austin1@gmail.com>
2016-02-18 20:00:03 -0600

    closer but not there yet

--- js/script.js
+++ js/script.js
@@ -1,15 +1,41 @@
 // $(document).ready(function() {
-//   $(".item").toggle("shake");
-// })
+//   if ($(".item") == "" && $(".item").keydown(function(event) {
+//     (event.which == 13) {
+//       $(".item").toggle("shake"); 
+//     }
+//   })
 
 $(document).ready(function() {
 
-  var listOfItems = [];
-  listOfItems.push(document.getElementById(listOfItems));
+  // for(i=0; i<=10; i++ ) {
+  //   document.write("hello <br/>");
+  // };
+
+  // if (".item" == " ") {
+  //   $(".item").toggle("shake");
+  // };
+
+  // var listOfItems = [];
+
+  // for (var i = 0; i < listOfItems.length; i++) {
+  //   console.log(listOfItems[i]);
+  // };
+
+  $( "input" )
+    .keyup(function() {
+      var value = $( this ).val();
+      $( "li" ).html( "<input type='checkbox' class='listOfItemsCheckbox'> " + value + " </input>" )
+    });
 
   $( "addBtn" ).click(function() {
-    $( "li" ).append( "<input type='checkbox'>" + listOfItems + "</input>" )
-    .addClass("listOfItemsCheckbox");
+    $( "li" ).append(function() {
+      var listOfItems = [];
+
+      for (var i = 0; i < listOfItems.length; i++) {
+        console.log(listOfItems[i]);
+      };
+    });
+
   });
 
 });
