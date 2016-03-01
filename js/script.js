581b5805361e5520efd2123b5364fbb65ae22bfd
Gabe Austin <gabriel.austin1@gmail.com>
2016-02-29 19:10:53 -0600

    clean up code

--- js/script.js
+++ js/script.js
@@ -1,32 +1,26 @@
-// $(document).ready(function() {
-//   if ($(".item") == "" && $(".item").keydown(function(event) {
-//     (event.which == 13) {
-//       $(".item").toggle("shake"); 
-//     }
-//   })
-
 $(document).ready(function() {
-
-  // for(i=0; i<=10; i++ ) {
-  //   document.write("hello <br/>");
-  // };
-
-  // if (".item" == " ") {
-  //   $(".item").toggle("shake");
-  // };
-
-  // var listOfItems = [];
-
-  // for (var i = 0; i < listOfItems.length; i++) {
-  //   console.log(listOfItems[i]);
-  // };
-
+  // takes input from the user
   $("input")
     .keyup(function() {
       var value = $(this).val();
       $("li:first-of-type").html("<label for=" + value + "><input type='checkbox' class='listOfItemsCheckbox' id=" + value + ">" + value + "</label>")
     });
-
+    // allows user to drag and drop unwanted items
+    $(".removeItem").droppable({
+      drop: function(event, ui) {
+        $(this)
+        .find(".deleteTrashCan")
+        .html("&nbsp; Item Removed!");
+        $(ui.draggable).remove();
+      }
+    });
+    // confirms that the user wants to remove this item
+    $(".removeItem").click(function(){
+      if(confirm("Are you sure you want to delete this?")){
+        $("input[type=checkbox]:checked").parent().remove();
+      }
+    })
+  // once user adds input and click the plus sign icon the icon is added to array
   $(".addBtn").click(function() {
     var listOfItems = [];
 
@@ -34,62 +28,18 @@ $(document).ready(function() {
         if (listOfItems[i] === $("input").val()) {
           $("li:first-of-type").remove()
         }
-      };
-        
-  $("ul").prepend("<li></li>");
-
-  $("input").val("").focus();
-
-  $(function() {
-    $("li").draggable();
-    $(".removeItem").droppable({
-      drop: function(event, ui) {
-        $(this)
-        .find(".deleteTrashCan")
-        .html("&nbsp; Item Removed!");
+      }
+      $("li:first-of-type").draggable();
+      $("ul").prepend("<li></li>");
+      $("input").val("").focus();
+    });
+    // adds line-through if user clicks checkbox
+    $("input:checkbox").change(function(){
+      if ($(this).is(":checked")) {
+        $(this).parent().addClass("addedList");
+      } else {
+        $(this).parent().removeClass("addedList");
       }
     });
   });
-
-  // needs work - below
-  $("input:checkbox").change(function(){
-    if ($(this).is(":checked")) {
-      $("li").addClass("addedList");
-    } else {
-      $("li").removeClass("addedList");
-    }
-  });
-
-
-  function removeItems(elem){
-    if(confirm("Are you sure you want to delete this?")){
-      elem.remove();
-    }
-  };
-
-  function itemClicked() {
-    $("item:checkbox");
-  };
-
-  function trashClicked() {
-    $(".deleteTrashCan").click();
-  };
-
-  if (itemClicked && trashClicked) {
-    removeItems($("li").find("input:checked").parent());
-  };
-
-  //delete checked items
-  // if ("li").click() && ("i").click()
-  // $("li").click(function() {
-  //   removeItems($("li").find("input:checked").parent());
-  // });
-
         
-  // console.log(listOfItems[i]);
-
-  });
-
-});
-
-
